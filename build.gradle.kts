4c4487f8f5d0c1f1183777af4a0d6010d3595629
William Blake Galbreath <Blake.Galbreath@GMail.com>
2026-01-14 13:44:48 -0600

    process resources during build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@ plugins {
     `java-library`
 }
 
-group = "net.pl3x"
+group = "net.pl3x.hytaletest"
 version = "1.0.0-SNAPSHOT"
+description = "Hytale test plugin"
 
 repositories {
     mavenCentral()
@@ -12,4 +13,24 @@ repositories {
 dependencies {
     // todo - replace with maven repo when available
     implementation(files(System.getenv("HYTALE_SERVER")))
+}
+
+tasks {
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+
+        // work around IDEA-296490
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        with(copySpec {
+            include("manifest.json")
+            from("src/main/resources") {
+                expand(
+                    "artifact" to "${rootProject.name}",
+                    "group" to "${rootProject.group}",
+                    "version" to "${rootProject.version}",
+                    "description" to "${rootProject.description}",
+                )
+            }
+        })
+    }
 }
